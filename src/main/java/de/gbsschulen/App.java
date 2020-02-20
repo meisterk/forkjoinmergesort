cfdc7474428416b8468d53813b1989cd536a9a14
meisterk <franz@kohnlehome.de>
2020-02-20 12:26:12 +0100

    Add imports

--- src/main/java/de/gbsschulen/App.java
+++ src/main/java/de/gbsschulen/App.java
@@ -1,5 +1,10 @@
 package de.gbsschulen;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hallo");
     }
 }
