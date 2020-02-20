6195eccc35fc48dea7cdfb819fe4777aa6332dbd
meisterk <franz@kohnlehome.de>
2020-02-20 12:18:20 +0100

    Initial commit

---
+++ src/main/java/de/gbsschulen/App.java
@@ -0,0 +1,13 @@
+package de.gbsschulen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
